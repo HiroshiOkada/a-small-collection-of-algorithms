b67d47b77036621e944d0e3d371ca2aa2f32ce48
Hiroshi Okada <okadahiroshi@miobox.jp>
2020-09-14 18:35:37 +0900

    factorize.py: 最初に素数を求めておくことによる版を追加

--- factorize.py
+++ factorize.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+import math
+import numpy as np
+import time
 
 
 def factorize(n: int):
@@ -29,7 +32,58 @@ def factorize(n: int):
     return arr
 
 
+class Factorize:
+    """最初に素数を求めてから素因数分解する"""
+
+    def __init__(self, max_n):
+        """max_n: 因数分解されると想定される最大の整数"""
+        self.sieve(math.floor(math.sqrt(max_n)))
+        return
+
+    def sieve(self, n):
+        """n以下の素数を計算"""
+        if n < 2:
+            self.primes = []
+            return
+        self.primes = [2]
+        if n == 2:
+            return
+        flags = np.array([True for i in range(3, n + 1, 2)])
+        for i in range(len(flags)):
+            if flags[i]:
+                prime = i * 2 + 3
+                self.primes.append(prime)
+                flags[i::prime] = False
+        return
+
+    def factorize(self, n):
+        """nを素因数分解する"""
+        arr = []
+        for prime in self.primes:
+            if prime * prime > n:
+                if prime != 1:
+                    arr.append((n, 1))
+                    return arr
+            count = 0
+            while n % prime == 0:
+                count += 1
+                n = n // prime
+            if count > 0:
+                arr.append((prime, count))
+
+
 if __name__ == "__main__":
-    n = 2**4 * 1000000007
+    n = 2 ** 4 * 1000000007
     print(n)
+    t0 = time.time()
     print(factorize(n))
+    print(time.time() - t0)
+
+    print('init')
+    t0 = time.time()
+    f = Factorize(n)
+    print(time.time() - t0)
+    print('init done')
+    t0 = time.time()
+    print(f.factorize(n))
+    print(time.time() - t0)
